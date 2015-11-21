296a4cf9843704a686f3bb295f4dd5b037fc512c
eaho1 <eaho1@uci.edu>
2015-11-20 23:24:56 -0800

    Created Edwin Scene + Cardboard SDK + Cardboard Demo

---
+++ Shut Up and Escape With Me/Assets/Scripts/Direction.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Direction : MonoBehaviour {
+    public KeyValuePair<int, int> up = new KeyValuePair<int, int>(0, -1);
+}
