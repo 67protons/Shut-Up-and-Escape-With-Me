4125f0002635999890a58e371faf8253a279187c
pgtruong <petergtruong@gmail.com>
2015-11-22 08:38:43 -0800

    Warp Portal Fix + Taylor Swift DLC

--- Shut Up and Escape With Me/Assets/Scripts/ToHEAVEN.cs
+++ Shut Up and Escape With Me/Assets/Scripts/ToHEAVEN.cs
@@ -2,6 +2,32 @@
 using System.Collections;
 
 public class ToHEAVEN : MonoBehaviour {
+    public Maze maze;
+    
+    void Start()
+    {
+        float x = (maze.width - 1f) * 3f;
+        float y = 1.55f;
+        float z = (maze.height - 0.5f) * 3f;
+        this.transform.position = new Vector3(x, y, z);
+    }
+
+    private void Taylorswift(Collider Boyfriend)                      // She wears short skirts, Boyfriend.name wears t-shirts.
+    {
+        if (Boyfriend)                                                // Boyfriend.name looks... at Taylorswift.
+        {
+            Debug.Log("No New Album");                                // Cause Boyfriend.name now we got bad blood.
+        }
+        else                                                          // We, are never ever ever, colliding together.
+        {
+            bool newAlbum = true;                                     // Taylorswift.shake, shake shake.
+            while (newAlbum)                                          // Say Ex-Boyfriend.name will remember me.
+            {
+                GameObject blankSpace = GameObject.Find("Boyfriend"); // Marry me Taylorswift you'll never have to be alone.
+                blankSpace.BroadcastMessage(Boyfriend.name);          // And I'll write Boyfriend.name.
+            }
+        }
+    }
 
     void OnTriggerEnter(Collider hitObject) {
         if (hitObject.name == "Player")
@@ -9,4 +35,5 @@ public class ToHEAVEN : MonoBehaviour {
             Application.LoadLevel("Win");
         }
     }    
+
 }
